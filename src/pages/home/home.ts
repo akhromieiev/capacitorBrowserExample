44ebbf09fe053a072e1130ef8d2e2c436b565ea3
ARoman <qpbd@ukr.net>
2018-05-09 13:17:19 +0300

    init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Plugins } from '@capacitor/core';
+const { Browser } = Plugins;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
+export class HomePage {
+  url = 'https://akhromieiev.com';
+  
   constructor(public navCtrl: NavController) {
 
   }
 
+  async openURL(url){
+    await Browser.open({'url': url});
+  }
 }
